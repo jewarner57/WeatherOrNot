78976ded165cd77994408ed28f640ada55a7411d
Jonathan Warner <jewarner57@gmail.com>
2021-04-19 19:14:48 -0400

    feat: challenge 7 complete

--- index.js
+++ index.js
@@ -11,20 +11,43 @@ const zipInput = document.getElementById('zip')
 formEl.addEventListener('submit', (e) => {
   e.preventDefault()
   const zip = zipInput.value
-  getWeather('3ec9bf803e2be1709ff0a2b7dc503967', zip).then((w) => { console.log(w) })
+  getWeather('3ec9bf803e2be1709ff0a2b7dc503967', zip, "zip", 'imperial').then((w) => { console.log(w) })
 
 })
 
 // Functions
-async function getWeather(key, zip) {
-  const apiKey = key
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+async function getWeather(apiKey, location, locationType, units) {
+
+  const locationString = getLocationString(location, locationType)
+  const path = `https://api.openweathermap.org/data/2.5/weather?${locationString}&appid=${apiKey}&units=${units}`
+
+  console.log(path)
 
   const res = await fetch(path)
   const json = await res.json()
 
   return json
+}
+
+function getLocationString(location, locationType) {
+  let locationString = ""
+
+  switch (locationType) {
+    case "zip":
+      locationString = `zip=${location}`
+      break;
+    case "cityID":
+      locationString = `id=${location}`
+      break;
+    case "geocoordinates":
+      locationString = `lat=${location.lat}&lon=${location.lon}`
+      break;
+    case "cityName":
+    default:
+      locationString = `q=${location}`
+  }
+
+  return locationString
 }
 
 // // Functions
